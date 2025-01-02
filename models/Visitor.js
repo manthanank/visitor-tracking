145e4a89dc486e3f13cdb43bd60774d3bd60fa8f
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-01-02 12:14:51 +0530

    add timestamps option to visitor schema

--- models/Visitor.js
+++ models/Visitor.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
-const visitorSchema = new mongoose.Schema({
-  projectName: { type: String, required: true }, 
-  website: { type: String, required: true },
-  ipAddress: { type: String, required: true },
-});
+const visitorSchema = new mongoose.Schema(
+  {
+    projectName: { type: String, required: true },
+    website: { type: String, required: true },
+    ipAddress: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Visitor", visitorSchema);
